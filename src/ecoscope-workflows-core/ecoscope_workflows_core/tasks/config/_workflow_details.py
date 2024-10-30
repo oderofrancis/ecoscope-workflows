bbdaf9f931e40972bc8cafdcd60b7cfd273f3873
Alex Morling <atmorling@gmail.com>
2024-10-30 12:03:10 +0200

    add default value for image_url

--- src/ecoscope-workflows-core/ecoscope_workflows_core/tasks/config/_workflow_details.py
+++ src/ecoscope-workflows-core/ecoscope_workflows_core/tasks/config/_workflow_details.py
@@ -18,7 +18,7 @@ class WorkflowDetails:
 def set_workflow_details(
     name: Annotated[str, Field(description="The name of your workflow")],
     description: Annotated[str, Field(description="A description")],
-    image_url: Annotated[str, Field(description="An image url")],
+    image_url: Annotated[str, Field(description="An image url")] = None,
 ) -> WorkflowDetails:
     return WorkflowDetails(
         name=name,
