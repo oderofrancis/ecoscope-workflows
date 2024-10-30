d70c1604db5f4fa387548eabb061688b1e98230d
Alex Morling <atmorling@gmail.com>
2024-10-30 09:51:16 +0200

    workflow details task

---
+++ src/ecoscope-workflows-core/ecoscope_workflows_core/tasks/config/workflow_details.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Annotated
+
+from pydantic import Field
+from pydantic.json_schema import SkipJsonSchema
+
+from ecoscope_workflows_core.decorators import task
+
+
+@dataclass
+class WorkflowDetails:
+    name: str
+    description: str
+    image_url: str | SkipJsonSchema[None] = None
+
+
+@task
+def set_workflow_details(
+    name: Annotated[str, Field(description="The name of your workflow")],
+    description: Annotated[str, Field(description="A description")],
+    image_url: Annotated[str, Field(description="An image url")],
+) -> WorkflowDetails:
+    return WorkflowDetails(
+        name=name,
+        description=description,
+        image_url=image_url,
+    )
